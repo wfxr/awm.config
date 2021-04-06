575a26ba07928165237e4ed35cfe5dcc460a141e
Wenxuan Zhang <wenxuangm@gmail.com>
2021-04-06 12:41:04 +0800

    update setup

--- setup.sh
+++ setup.sh
@@ -4,6 +4,18 @@ IFS=$'\n\t'
 
 SDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SDIR"
 
+CONF=~/.config/awesome
+mkdir -p ~/.config
+
+read -n1 -rp "$CONF will be removed. Continue? [y/N] " res; echo
+[[ ! $res =~ y|Y ]] && exit 1
+
+rm -rf "$CONF"
+ln -s "$SDIR/awesome" "$CONF"
+
+read -n1 -rp "Install dependencies? [y/N] " res; echo
+[[ ! $res =~ y|Y ]] && exit 1
+
 paru -S --noconfirm \
     awesome-git \
     rofi \
@@ -28,11 +40,3 @@ mkdir -p ~/.local/share/fonts
 cp -r ./fonts/* ~/.local/share/fonts
 
 fc-cache -fv
-
-CONF=~/.config/awesome
-mkdir -p ~/.config
-read -n1 -rp "$CONF will be removed. Continue? [y/N] " res; echo
-[[ ! $res =~ y|Y ]] && exit 1
-
-rm -rf "$CONF"
-ln -s "$SDIR/awesome" "$CONF"
